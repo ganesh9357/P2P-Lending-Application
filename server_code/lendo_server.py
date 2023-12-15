8c7e856b92fc1372f83467be80758e73aba7a439
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2023-12-15 07:27:55 +0000

    Edited lendo_server and lendor_registration_form.Lender_reg_form_6

--- server_code/lendo_server.py
+++ server_code/lendo_server.py
@@ -9,7 +9,7 @@ import anvil.server
 
 @anvil.server.callable
 def add_lendor_frist_form(name,gender,date_of_birth,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['full_name'] = name
     row[0]['gender'] = gender
@@ -18,7 +18,7 @@ def add_lendor_frist_form(name,gender,date_of_birth,user_id):
 
 @anvil.server.callable
 def add_lendor_second_form(mobile,email,photo,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     #row[0]['investment'] = investment
     row[0]['mobile'] = mobile
@@ -28,7 +28,7 @@ def add_lendor_second_form(mobile,email,photo,user_id):
 
 @anvil.server.callable
 def add_lendor_education_form(qualification,certificate,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     
     row[0]['qualification'] = qualification
@@ -37,7 +37,7 @@ def add_lendor_education_form(qualification,certificate,user_id):
 
 @anvil.server.callable
 def add_lendor_third_form(aadhaar_photo, pan_card, pan_id,aadhaar_card,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['pan_photo'] = pan_id
     row[0]['pan_number'] = pan_card
@@ -48,7 +48,7 @@ def add_lendor_third_form(aadhaar_photo, pan_card, pan_id,aadhaar_card,user_id):
 
 @anvil.server.callable
 def add_lendor_four_form(street_adress_1,street_address_2,city,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['street_adress_1'] = street_adress_1
     row[0]['street_address_2'] = street_address_2
@@ -58,7 +58,7 @@ def add_lendor_four_form(street_adress_1,street_address_2,city,user_id):
                           
 @anvil.server.callable
 def add_lendor_five_form(pincode,state,country,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['state'] = state
     row[0]['country'] = country
@@ -68,12 +68,12 @@ def add_lendor_five_form(pincode,state,country,user_id):
 
 @anvil.server.callable
 def add_lendor_six_form(lending_type, investment,lending_period, user_id):
-  row = app_tables.lender.add_row(investment=investment, lending_type=lending_type,lending_period=lending_period,coustmer_id=user_id)
+  row = app_tables.lender.add_row(investment=investment, lending_type=lending_type,lending_period=lending_period,customer_id = user_id)
     
     
 @anvil.server.callable
 def add_lendor_individual_form_1(company_name,org_type,emp_type,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['company_name']=company_name
     row[0]['organization_type']=org_type
@@ -83,7 +83,7 @@ def add_lendor_individual_form_1(company_name,org_type,emp_type,user_id):
 
 @anvil.server.callable
 def add_lendor_individual_form_2(business_phone_number, landmark,comp_address,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['business_no']=business_phone_number
     row[0]['company_landmark']=landmark
@@ -92,7 +92,7 @@ def add_lendor_individual_form_2(business_phone_number, landmark,comp_address,us
 
 @anvil.server.callable
 def add_lendor_individual_form_3(annual_salary, designation,emp_id_proof,last_six_month,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row [0]['annual_salary']=annual_salary
     row[0]['designation'] = designation
@@ -101,7 +101,7 @@ def add_lendor_individual_form_3(annual_salary, designation,emp_id_proof,last_si
 
 @anvil.server.callable
 def add_lendor_institutional_form_1(business_name,business_location,business_add,branch_name,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['business_name'] = branch_name
     row[0]['business_location'] = business_location
@@ -110,7 +110,7 @@ def add_lendor_institutional_form_1(business_name,business_location,business_add
 
 @anvil.server.callable
 def add_lendor_institutional_form_2(nearest_loc,business_type,empolyees_working,year_estd,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['nearest_location'] = nearest_loc
     row[0]['business_type'] = business_type
@@ -119,7 +119,7 @@ def add_lendor_institutional_form_2(nearest_loc,business_type,empolyees_working,
 
 @anvil.server.callable
 def add_lendor_institutional_form_3(industry_type,six_monthturnover,last_six_statments,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['industry_type'] = industry_type
     row[0]['six_month_turnover'] = six_monthturnover
@@ -127,7 +127,7 @@ def add_lendor_institutional_form_3(industry_type,six_monthturnover,last_six_sta
 
 @anvil.server.callable
 def add_lendor_institutional_form_4(director_name,director_no,din,cin,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['director_name'] = director_name
     row[0]['director_no'] = director_no
@@ -136,7 +136,7 @@ def add_lendor_institutional_form_4(director_name,director_no,din,cin,user_id):
 
 @anvil.server.callable
 def add_lendor_institutional_form_5(reg_office_add,off_add_proof,proof_verification,user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['registered_off_add'] = reg_office_add
     row[0]['off_add_proof'] = off_add_proof
@@ -144,7 +144,7 @@ def add_lendor_institutional_form_5(reg_office_add,off_add_proof,proof_verificat
 
 @anvil.server.callable
 def add_lendor_bank_details_form_1(account_name, account_type,account_number,bank_name, user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['account_name'] = account_name
     row[0]['account_type'] = account_type
@@ -154,7 +154,7 @@ def add_lendor_bank_details_form_1(account_name, account_type,account_number,ban
 
 @anvil.server.callable
 def add_lendor_bank_details_form_2(ifsc,salary_type,branch_name,net_bank, user_id):
-  row = app_tables.user_profile.search(coustmer_id=user_id)
+  row = app_tables.user_profile.search(customer_id = user_id)
   if row:
     row[0]['ifsc_code'] = ifsc
     row[0]['salary_type'] = salary_type
@@ -187,7 +187,7 @@ def search_user(query):
   if query:
     result=[
     x for x in result
-    if query in x["coustmer_id"]
+    if query in x["customer_id"]
     ]
   return result
 
@@ -199,7 +199,7 @@ from anvil.tables import app_tables
 
 @anvil.server.callable
 def get_user_data(user_id):
-    user = app_tables.user_profile.get(coustmer_id=user_id)
+    user = app_tables.user_profile.get(customer_id = user_id)
     if user:
         return {
             'full_name': user['full_name'],
