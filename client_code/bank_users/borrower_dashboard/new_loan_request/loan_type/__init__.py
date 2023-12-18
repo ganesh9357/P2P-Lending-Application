004481f6e57eb14388178bec7287dddc639d9270
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2023-12-18 08:27:22 +0000

    Edited bank_users.borrower_dashboard.new_loan_request.loan_type

--- client_code/bank_users/borrower_dashboard/new_loan_request/loan_type/__init__.py
+++ client_code/bank_users/borrower_dashboard/new_loan_request/loan_type/__init__.py
@@ -25,14 +25,14 @@ class loan_type(loan_typeTemplate):
         # Manually fetch loan types from the 'product_group' table
         loan_types = app_tables.product_group.search()
 
-        # Populate the dropdown with fetched loan types
-        if loan_types:
-            self.drop_down_1.items = [loan['name'] for loan in loan_types]
-            self.drop_down_1.selected_value = loan_types[0]['name']  # Set the default selection
-        else:
-          # Handle the case where loan_types is empty (optional)
-          self.drop_down_1.items = ["No loan types available"]
-          self.drop_down_1.selected_value = None
+        # # Populate the dropdown with fetched loan types
+        # if loan_types:
+        #     self.drop_down_1.items = [loan['name'] for loan in loan_types]
+        #     self.drop_down_1.selected_value = loan_types[0]['name']  # Set the default selection
+        # else:
+        #   # Handle the case where loan_types is empty (optional)
+        #   self.drop_down_1.items = ["No loan types available"]
+        #   self.drop_down_1.selected_value = None
 
     def populate_product_categories(self):
         # Manually fetch product categories from the 'product_category' table
