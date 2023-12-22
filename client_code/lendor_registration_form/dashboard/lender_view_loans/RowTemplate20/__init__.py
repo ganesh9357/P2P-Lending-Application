5c394766bb219fbf4749a51c598641233849cf2e
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-22 08:42:01 +0000

    Edited lendor_registration_form.dashboard.lender_view_loans.RowTemplate20

--- client_code/lendor_registration_form/dashboard/lender_view_loans/RowTemplate20/__init__.py
+++ client_code/lendor_registration_form/dashboard/lender_view_loans/RowTemplate20/__init__.py
@@ -14,3 +14,7 @@ class RowTemplate20(RowTemplate20Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def view_profile_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+
