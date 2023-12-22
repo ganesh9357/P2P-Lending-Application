7fc4886a7c8fd9fe8fce6a953f398e465b3c1a5b
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-22 05:25:56 +0000

    Edited lendor_registration_form.dashboard.lender_view_loans.RowTemplate24, lendor_registration_form.dashboard.lender_view_loans.view_details_1 and lendor_registration_form.dashboard.lender_view_loans

--- client_code/lendor_registration_form/dashboard/lender_view_loans/__init__.py
+++ client_code/lendor_registration_form/dashboard/lender_view_loans/__init__.py
@@ -96,4 +96,3 @@ class lender_view_loans(lender_view_loansTemplate):
   def home_lender_registration_form_copy_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form("lendor_registration_form.dashboard")
-
--- client_code/lendor_registration_form/dashboard/lender_view_loans/view_details_1/__init__.py
+++ client_code/lendor_registration_form/dashboard/lender_view_loans/view_details_1/__init__.py
@@ -14,3 +14,21 @@ class view_details_1(view_details_1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    # Display loan details
+    self.loan_id_label.text = f"{selected_row['loan_id']}"
+    self.loan_amount_label.text=f"{selected_row['loan_amount']}"
+    self.intrest_rate_label.text=f"{selected_row['interest_rate']}"
+    self.tenure_label.text=f"{selected_row['tenure']}"
+    timestamp = selected_row['timestamp']
+    date_of_apply = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+    self.date_of_apply_label.text = f"{date_of_apply}"
+    self.loan_updated_status_label.text=f"{selected_row['loan_updated_status']}"
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.lender_view_loans")
+
+  def button_1_copy_click(self, **event_args):
+    """This method is called when the button is clicked"""
+
+
