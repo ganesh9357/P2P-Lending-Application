c250c9a06aa32a226d08ed4c851e812f9648312d
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2023-12-25 07:58:25 +0000

    Edited admin.dashboard.manage_products.view_profile

--- client_code/admin/dashboard/manage_products/view_profile/__init__.py
+++ client_code/admin/dashboard/manage_products/view_profile/__init__.py
@@ -21,11 +21,16 @@ class view_profile(view_profileTemplate):
     self.categories_list = []
     self.profee_list = []
     self.extfee_list = []
+    self.foreclose_list = []
+    self.extension_list = []
     self.type_list = []
     self.int_type = []
-    self.max_days = []
-    self.min_days = []
     self.roi = []
+    self.max_amount = []
+    self.min_amount = []
+    self.min_tenure_list = []
+    self.max_tenure_list = []
+    self.emi_payment = []
     self.dis_cou = []
 
     a = -1
@@ -36,11 +41,16 @@ class view_profile(view_profileTemplate):
       self.categories_list.append(i['product_categories'])
       self.profee_list.append(i['processing_fee'])
       self.extfee_list.append(i['extension_fee'])
+      self.foreclose_list.append(i['foreclose_type'])
+      self.extension_list.append(i['extension_allowed'])
       self.type_list.append(i['membership_type'])
       self.int_type.append(i['interest_type'])
-      self.max_days.append(i['max_days'])
-      self.min_days.append(i['min_days'])
       self.roi.append(i['roi'])
+      self.max_amount.append(i['max_amount'])
+      self.min_amount.append(i['min_amount'])
+      self.min_tenure_list.append(i['min_tenure'])
+      self.max_tenure_list.append(i['max_tenure'])
+      self.emi_payment.append(i['emi_payment'])
       self.dis_cou.append(i['discount_coupons'])
 
     if a == -1:
@@ -53,11 +63,16 @@ class view_profile(view_profileTemplate):
         self.label_4.text = self.categories_list[b]
         self.label_5.text = self.profee_list[b]
         self.label_6.text = self.extfee_list[b]
+        self.label_20.text = self.foreclose_list[b]
+        self.label_22.text = self.extension_list[b]
         self.label_7.text = self.type_list[b]
         self.label_11.text = self.int_type[b]
-        self.label_8.text = self.max_days[b]
-        self.label_9.text = self.min_days[b]
+        self.label_8.text = self.min_amount[b]
+        self.label_9.text = self.max_amount[b]
+        self.label_15.text = self.min_tenure_list[b]
+        self.label_16.text = self.max_tenure_list[b]
         self.label_10.text = self.roi[b]
+        self.label_18.text = self.emi_payment[]
         self.label_12.text = self.dis_cou[b]
 
   def button_1_click(self, **event_args):
