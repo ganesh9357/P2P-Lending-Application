27006aadd828a64688e7be02a347be15eb2a44f3
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2023-12-25 07:04:51 +0000

    Edited settings, admin.dashboard.manage_products.manage_producs1, admin_server and admin.dashboard.manage_products.view_profile

--- client_code/admin/dashboard/manage_products/manage_producs1/__init__.py
+++ client_code/admin/dashboard/manage_products/manage_producs1/__init__.py
@@ -9,89 +9,110 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class manage_producs1(manage_producs1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-    self.id = 'A' + str(1000000)  
-    self.label_1.text = self.id
-    self.data = tables.app_tables.product_details.search()
+        # Any code you write here will run before the form opens.
+        self.id = 'PD' + str(1000)  
+        self.label_1.text = self.id
+        self.data = tables.app_tables.product_details.search()
 
-    a = -1
-    self.list_1 = []
- 
-    for i in self.data:
-      a+=1
-      self.list_1.append(i['product_id']) 
-    if a == -1 :
-      self.id = 'A' + str(1000000)
-      self.label_1.text = self.id
-      
-    else:
-      self.id = 'A'+ str(int(self.list_1[-1][1:])+1)
-      self.label_1.text = self.id
-   
-  def link_1_copy_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('admin.dashboard.manage_products')
+        a = -1
+        self.list_1 = []
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
+        for i in self.data:
+            a += 1
+            self.list_1.append(i['product_id']) 
+        if a == -1:
+            self.id = 'PD' + str(1000)
+            self.label_1.text = self.id
+        else:
+            last_product_id = self.list_1[-1]
+            numeric_part = last_product_id[2:]  # Assuming the prefix is always two characters ('PD')
+            self.id = 'PD' + str(int(numeric_part) + 1)
+            self.label_1.text = self.id
 
+    def link_1_copy_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        open_form('admin.dashboard.manage_products')
 
-    product_name = self.text_box_2.text
-    product_discription = self.text_area_1.text
-    processing_fee = int(self.text_box_3.text)
-    extension_fee = int(self.text_box_4.text)
-    discount_coupons = self.radio_button_3.text
-    membership_type = self.drop_down_2.selected_value
-    print(membership_type)
-    interest_type = self.radio_button_1.text
-    if interest_type :
-      print(self.radio_button_1.text)
-    else :
-      print(self.radio_button_2.text)
-    min_days = int(self.drop_down_3.selected_value)
-    print(min_days)
-    max_days = int(self.drop_down_4.selected_value)
-    print(max_days)
-    roi = int(self.text_box_5.text)
-    print(roi)
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
 
-    if product_name == "" or membership_type == "" or processing_fee == "" or extension_fee == "" or interest_type == "" or max_days == "" or min_days == ""  or discount_coupons == "" or roi == "":
-      Notification("Fill All Required Details").show()
-    else:
-     anvil.server.call('product_details', self.id, product_name, processing_fee, extension_fee, membership_type, interest_type, max_days, min_days, roi, discount_coupons)
-     product_id = self.label_1.text
-     open_form('admin.dashboard.manage_products.add_group',product_id )
- 
+        product_name = self.text_box_2.text
+        product_discription = self.text_area_1.text
+        product_categories = self.product_category.text
+        processing_fee = int(self.text_box_3.text)
+        extension_fee = int(self.text_box_4.text)
+        membership_type = self.drop_down_2.selected_value
+        print(membership_type)
+        interest_type = self.radio_button_1.text
+        if interest_type:
+            print(self.radio_button_1.text)
+        else:
+            print(self.radio_button_2.text)
+        min_amount = int(self.min_amount.text)
+        print(min_amount)
+        max_amount = int(self.max_amount.text)
+        print(max_amount)
+        min_tenure = int(self.min_tenure.text)
+        max_tenure = int(self.max_tenure.text)
+        roi = int(self.text_box_5.text)
+        print(roi)
+        foreclose_type = self.foreclose_type.selected_value
+        extension_allowed_mapping = {
+    'Yes': True,
+    'No': False,
+}
+        extension_allowed = extension_allowed_mapping.get(self.extension_allowed.selected_value, False)
+        emi_payment = self.drop_down_1.selected_value
+        print(emi_payment)
+        discount_coupons = self.radio_button_3.text
+        if discount_coupons:
+          print(self.radio_button_3.text)
+        else:
+          print(self.radio_button_4.text)
 
-  def check_box_3_change(self, **event_args):
-    """This method is called when this checkbox is checked or unchecked"""
-    product_visibility = self.check_box_3.checked
-    if  product_visibility :
-      self.label_1.visible = True
-      self.text_box_2.visible = True
-      self.text_area_1.visible = True
-      self.text_box_3.visible = True
-      self.text_box_4.visible = True
-      self.drop_down_2.visible = True
-      self.radio_button_1.visible = True
-      self.drop_down_3.visible = True
-      self.drop_down_4.visible =True
-      self.text_box_5.visible = True
-      self.radio_button_3.visible = True
-      self.radio_button_4.visible = True
-    else:
-      self.text_box_2.visible = False
-      self.text_area_1.visible = False
-      self.text_box_3.visible = False
-      self.text_box_4.visible = False
-      self.drop_down_2.visible = False
-      self.radio_button_1.visible = False
-      self.drop_down_3.visible = False
-      self.drop_down_4.visible =False
-      self.text_box_5.visible = False
-      self.radio_button_3.visible = False
-      self.radio_button_4.visible = False
+        if product_name == "" or product_categories== "" or membership_type == "" or processing_fee == "" or extension_fee == "" or interest_type == "" or max_amount == "" or min_amount == "" or min_tenure == "" or max_tenure == "" or roi == "" or emi_payment == "" or discount_coupons == "":
+            Notification("Fill All Required Details").show()
+        else:
+            anvil.server.call('product_details', self.id, product_name, product_categories, processing_fee, extension_fee, membership_type, interest_type, max_amount, min_amount, min_tenure, max_tenure, roi, foreclose_type, extension_allowed, emi_payment, discount_coupons)
+            product_id = self.label_1.text
+            # open_form('admin.dashboard.manage_products.add_group',product_id )
+
+    def check_box_3_change(self, **event_args):
+        """This method is called when this checkbox is checked or unchecked"""
+        product_visibility = self.check_box_3.checked
+        if product_visibility:
+            self.label_1.visible = True
+            self.text_box_2.visible = True
+            self.text_area_1.visible = True
+            self.text_box_3.visible = True
+            self.text_box_4.visible = True
+            self.drop_down_2.visible = True
+            self.radio_button_1.visible = True
+            self.min_amount.visible = True
+            self.max_amount.visible = True
+            self.max_tenure.visible = True
+            self.min_tenure.visible = True
+            self.text_box_5.visible = True
+            self.radio_button_3.visible = True
+            self.radio_button_4.visible = True
+            self.emi_paymentt.visible = True
+        else:
+            self.label_1.visible = False
+            self.text_box_2.visible = False
+            self.text_area_1.visible = False
+            self.text_box_3.visible = False
+            self.text_box_4.visible = False
+            self.drop_down_2.visible = False
+            self.radio_button_1.visible = False
+            self.min_amount.visible = False
+            self.max_amount.visible = False
+            self.max_tenure.visible = False
+            self.min_tenure.visible = False
+            self.text_box_5.visible = False
+            self.radio_button_3.visible = False
+            self.radio_button_4.visible = False
+            self.emi_paymentt.visible = False
