f10d1f8f455a7f0117d3584563fc99105736eeb9
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-27 18:04:22 +0000

    Edited bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure

--- client_code/bank_users/borrower_dashboard/borrower_foreclosure_request/borrower_foreclosure/__init__.py
+++ client_code/bank_users/borrower_dashboard/borrower_foreclosure_request/borrower_foreclosure/__init__.py
@@ -19,6 +19,8 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
         self.label_interest_rate.text = f"{selected_row['interest_rate']} % pa"
         self.label_credit_limit.text = f"{selected_row['credit_limit']}"
         self.label_tpm.text = f"{selected_row['borrower_last_payment_done']}"
+        self.text_box_1.text = "Foreclosure Request Under Process......"  
+        self.text_box_2.text = "Foreclosure Request Rejected"
 
         # Check foreclosure status for the selected loan ID
         loan_id = selected_row['loan_id']
@@ -45,10 +47,9 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
             # If there is a reject status, show an alert
             alert('Your request has been rejected.')
             self.button_foreclose.visible = False
-            self.button_2.visible = False
+            self.button_2.visible = True
             self.button_3.visible = False
-            self.button_7.visible = True
-            self.button_8.visible = True
+            self.text_box_2.visible = True
         else:
             # If there is no approved or reject status, check if the loan ID is in foreclosure table
             existing_requests = app_tables.foreclosure.search(loan_id=loan_id)
@@ -58,16 +59,14 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
                 self.button_2.visible = True
                 self.button_3.visible = False
                 self.button_4.visible = False
-                self.button_5.visible = False
-                self.button_6.visible = False
+                
             else:
                 # If the loan ID is in the foreclosure table, make other buttons visible
                 self.button_foreclose.visible = False
-                self.button_2.visible = False 
+                self.button_2.visible = True 
                 self.button_4.visible = False
                 self.button_3.visible = False
-                self.button_5.visible = True
-                self.button_6.visible = True 
+                self.text_box_1.visible = True
 
         # Save selected_row as an instance variable for later use
         self.selected_row = selected_row
@@ -77,13 +76,6 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
       loan_id = selected_row['loan_id']
 
       borrower_last_payment_done  = selected_row['borrower_last_payment_done']
-      
-
-      # if borrower_last_payment_done :
-      #   open_form('bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure.foreclose', selected_row=selected_row)
-      # else:
-      #   alert('You are not eligible for foreclosure! You have to pay at least 12 months.')
-      #   open_form('bank_users.borrower_dashboard.borrower_foreclosure_request')
 
       if borrower_last_payment_done > 12:
             open_form('bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure.foreclose', selected_row=selected_row)
@@ -103,10 +95,3 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
         """This method is called when the button is clicked"""
         open_form('bank_users.borrower_dashboard.borrower_foreclosure_request')
 
-    def button_5_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        open_form('bank_users.borrower_dashboard.borrower_foreclosure_request')
-
-    def button_7_click(self, **event_args):
-      """This method is called when the button is clicked"""
-      open_form('bank_users.borrower_dashboard.borrower_foreclosure_request')
