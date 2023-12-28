e950e3be94f1be790e44ad9f2315e5ee9ebb806e
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-28 17:02:31 +0000

    Edited admin.dashboard.manage_products.add_product_categories_and_groups

--- client_code/admin/dashboard/manage_products/add_product_categories_and_groups/__init__.py
+++ client_code/admin/dashboard/manage_products/add_product_categories_and_groups/__init__.py
@@ -18,16 +18,36 @@ class add_product_categories_and_groups(add_product_categories_and_groupsTemplat
     self.drop_down_1.items = option_strings
     self.drop_down_1.selected_value = option_strings[0] if option_strings else None   
 
+  # def name_change(self, **event_args):
+
+  #       # Check the selected value of drop_down_1
+  #       selected_group = self.drop_down_1.selected_value
+
+  #       # Make additional features visible if a group is selected
+  #       if selected_group:
+  #           self.label_2.visible = True
+  #           self.drop_down_2.visible = True
+  #       else:
+  #           # Hide additional features if no group is selected
+  #           self.label_2.visible = False
+  #           self.drop_down_2.visible = False
+
   def name_change(self, **event_args):
+        self.selected_value = self.drop_down_1.selected_value
 
-        # Check the selected value of drop_down_1
-        selected_group = self.drop_down_1.selected_value
+        if self.selected_value:
+          self.label_1.visible = True
+          self.label_2.visible = True
+          self.drop_down_1.visible = True
+          self.drop_down_2.visible = True
 
-        # Make additional features visible if a group is selected
-        if selected_group:
-            self.label_2.visible = True
-            self.drop_down_2.visible = True
-        else:
-            # Hide additional features if no group is selected
-            self.label_2.visible = False
-            self.drop_down_2.visible = False
+            # Fetch product categories based on the selected loan type
+          product_categories = app_tables.product_categories.search(
+          name_group=self.selected_value
+          )
+
+          if product_categories:
+            # Display product categories in drop_down_2
+            self.drop_down_2.items = [category['name_categories'] for category in product_categories]
+            self.drop_down_2.selected_value = product_categories[0]['name_categories'] if product_categories else None
+ 
