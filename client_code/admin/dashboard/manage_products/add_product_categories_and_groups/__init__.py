5512876ba11077451df195e72d3c98ff0df17c63
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-29 06:54:35 +0000

    Edited settings and admin.dashboard.manage_products.add_product_categories_and_groups

--- client_code/admin/dashboard/manage_products/add_product_categories_and_groups/__init__.py
+++ client_code/admin/dashboard/manage_products/add_product_categories_and_groups/__init__.py
@@ -19,22 +19,40 @@ class add_product_categories_and_groups(add_product_categories_and_groupsTemplat
     self.drop_down_1.selected_value = option_strings[0] if option_strings else None   
 
 
-  def name_change(self, **event_args):
-        self.selected_value = self.drop_down_1.selected_value
+  # def name_change(self, **event_args):
+  #       self.selected_value = self.drop_down_1.selected_value
 
-        if self.selected_value:
-          self.label_1.visible = True
-          self.label_2.visible = True
-          self.drop_down_1.visible = True
-          self.text_box_1.visible = True
+  #       if self.selected_value:
+  #         self.label_1.visible = True
+  #         self.label_2.visible = True
+  #         self.drop_down_1.visible = True
+  #         self.text_box_1.visible = True
 
-            # Fetch product categories based on the selected loan type
-          product_categories = app_tables.product_categories.search(
-          name_group=self.selected_value
-          )
+  #           # Fetch product categories based on the selected loan type
+  #         product_categories = app_tables.product_categories.search(
+  #         name_group=self.selected_value
+  #         )
 
-          if product_categories:
-            # Display product categories in drop_down_2
-            self.text_box_1.items = [category['name_categories'] for category in product_categories]
-            self.text_box_1.selected_value = product_categories[0]['name_categories'] if product_categories else None
 
+  def button_2_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        # Get selected values
+        selected_group = self.drop_down_1.selected_value
+        text_box_value = self.text_box_1.text
+
+        # Check if both values are selected or entered
+        if selected_group and text_box_value:
+            # Create a new row in the product_categories table and set the values
+            app_tables.product_categories.add_row(
+                name_group=selected_group,
+                name_categories=text_box_value
+            )
+            # Optionally, you can show a confirmation message
+            alert("Product category saved successfully!")
+            # Clear the input fields after saving
+            self.drop_down_1.selected_value = None
+            self.text_box_1.text = ""
+            open_form('admin.dashboard.manage_products')
+        else:
+            # Show an error message if one or both values are not selected or entered
+            alert("Please enter/select all details before saving.")
