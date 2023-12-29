93e3bb8daa694b3770a765b3aecfe532ff59cfd8
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-29 09:47:07 +0000

    Edited admin_server and admin.dashboard.manage_products.add_group

--- client_code/admin/dashboard/manage_products/add_group/__init__.py
+++ client_code/admin/dashboard/manage_products/add_group/__init__.py
@@ -12,20 +12,15 @@ class add_group(add_groupTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # self.text_box_1 = TextBox()
 
     # Any code you write here will run before the form opens.
   def add_button_click(self, **event_args):
         """This method is called when the button is clicked"""
         # Get the text entered in the TextBox
         group_name = self.text_box_1.text
-        # Check if the entered group_name is not empty
         if group_name:
-            # Add the group_name to the "product_group" data table
             app_tables.product_group.add_row(name=group_name)
-            # Optionally, you can clear the TextBox after adding the group
             self.text_box_1.text = ''
-            # Optionally, provide feedback to the user
             alert(f' "{group_name}" added successfully!')
             open_form('admin.dashboard.manage_products.add_product_categories_and_groups')
 
