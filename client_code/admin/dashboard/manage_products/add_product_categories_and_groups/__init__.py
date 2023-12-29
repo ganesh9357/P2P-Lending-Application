ab1bcbaeedd0aa41babe55697e91f0222dc135cc
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2023-12-29 05:49:53 +0000

    Edited settings, admin.dashboard.manage_products.add_product_categories_and_groups and admin.dashboard.manage_products.add_group

--- client_code/admin/dashboard/manage_products/add_product_categories_and_groups/__init__.py
+++ client_code/admin/dashboard/manage_products/add_product_categories_and_groups/__init__.py
@@ -26,7 +26,7 @@ class add_product_categories_and_groups(add_product_categories_and_groupsTemplat
           self.label_1.visible = True
           self.label_2.visible = True
           self.drop_down_1.visible = True
-          self.drop_down_2.visible = True
+          self.text_box_1.visible = True
 
             # Fetch product categories based on the selected loan type
           product_categories = app_tables.product_categories.search(
@@ -35,32 +35,6 @@ class add_product_categories_and_groups(add_product_categories_and_groupsTemplat
 
           if product_categories:
             # Display product categories in drop_down_2
-            self.drop_down_2.items = [category['name_categories'] for category in product_categories]
-            self.drop_down_2.selected_value = product_categories[0]['name_categories'] if product_categories else None
+            self.text_box_1.items = [category['name_categories'] for category in product_categories]
+            self.text_box_1.selected_value = product_categories[0]['name_categories'] if product_categories else None
 
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('admin.dashboard.manage_products')
-    
-  def button_2_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        # Get selected values
-        selected_group = self.drop_down_1.selected_value
-        selected_category = self.drop_down_2.selected_value
-
-        # Check if both values are selected
-        if selected_group and selected_category:
-            # Create a new row in the product_details table and set the values
-            app_tables.product_details.add_row(
-                product_name=selected_group,
-                product_categories=selected_category
-            )
-
-            # Optionally, you can show a confirmation message
-            alert("Product details saved successfully!")
-
-            # Navigate to the manage_products form
-            open_form('admin.dashboard.manage_products')
-        else:
-            # Show an error message if one or both values are not selected
-            alert("Please select both product group and product category before saving.")
