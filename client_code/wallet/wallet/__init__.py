048aa2f17dab5e29ed10ca16f032c957a0a593c7
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2023-12-29 12:02:03 +0000

    Edited wallet.wallet

--- client_code/wallet/wallet/__init__.py
+++ client_code/wallet/wallet/__init__.py
@@ -8,7 +8,6 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from anvil import open_form, server
-# from ....bank_users.main_form import main_form_module
 from ...lendor_registration_form.dashboard import lendor_main_form_module as main_form_module
 
 
@@ -19,6 +18,7 @@ class wallet(walletTemplate):
     self.init_components(**properties)
     self.deposit_placeholder = "5000"
     self.withdraw_placeholder = "0.00"
+    
   
     self.user_id = main_form_module.userId
     user_id = self.user_id
@@ -47,8 +47,8 @@ class wallet(walletTemplate):
     # notification = anvil.Notification("Fetched Data successfully!", style="success")
     # notification.show()
 
-    # save the amount to e_wallet column
-    # customer_id = main_form_module.userId 
+    # save the amount to e_wallet column 
+
     # e_wallet = None
 
     # user_data = app_tables.wallet.search(customer_id=customer_id)
@@ -58,6 +58,24 @@ class wallet(walletTemplate):
 
     # if e_wallet:
     #   self.amount_text_box.text = e_wallet
+    # Fetch the user's profile
+    user_profile = app_tables.user_profile.get(customer_id=customer_id)
+
+    if user_profile is not None:
+      customer_id = user_profile['customer_id']
+
+      e_wallet = None
+      user_data = app_tables.wallet.search(customer_id=customer_id)
+
+      if user_data and len(user_data) > 0:
+        e_wallet = user_data[0]['e_wallet']
+
+      if e_wallet:
+        self.amount_text_box.text = str(e_wallet)
+      else:
+        # Handle if user profile doesn't exist
+        notification = anvil.Notification("User profile not found!", style="warning")
+        notification.show()
 
 
   def home_main_form_link_click(self, **event_args):
@@ -106,40 +124,57 @@ class wallet(walletTemplate):
 
   def deposit_money_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    customer_id = main_form_module.userId  
+    # selected_row = self.selected_row
+    # customer_id = selected_row['customer_id']
+    # e_wallet = selected_row['e_wallet']
 
     e_wallet = self.amount_text_box.text
-    
-    # Check if the entered amount is valid
+   
     if e_wallet:
-        try:
-            e_wallet_amount = int(e_wallet)  
-        except ValueError:
-            # Handle if the entered amount is not a valid integer
-            notification = anvil.Notification("Please enter a valid amount!", style="warning")
-            notification.show()
-            return
+      user_data = app_tables.wallet.search(customer_id=customer_id)
 
-        # Fetch the user's data from the wallet table
-        user_data = app_tables.wallet.search(customer_id=customer_id)
-
-        if user_data and len(user_data) > 0:
-            # If the row exists, update the e_wallet value
-            user_row = user_data[0]
-            user_row['e_wallet'] = e_wallet_amount
-            print("Updating existing row...")
-        else:
-            # If the row doesn't exist, add a new row
-            app_tables.wallet.add_row(customer_id=customer_id, e_wallet=e_wallet_amount)
-            print("Adding new row...")
-            
-        # Display a success message after updating or adding the e_wallet value
-        notification = anvil.Notification("Money Deposited successfully!", style="success")
-        notification.show()
+    if user_data and len(user_data) > 0:
+      user_row = user_data[0]
+      user_row['e_wallet'] = e_wallet
+    
     else:
-        # Notify if no amount is entered
-        notification = anvil.Notification("Please enter an amount!", style="warning")
-        notification.show()
+      # If the row doesn't exist, add a new row
+      app_tables.wallet.add_row(customer_id=customer_id, e_wallet=e_wallet)
+    
+    # customer_id = main_form_module.userId  
+
+    # e_wallet = self.amount_text_box.text
+    
+    # # Check if the entered amount is valid
+    # if e_wallet:
+    #     try:
+    #         e_wallet_amount = int(e_wallet)  
+    #     except ValueError:
+    #         # Handle if the entered amount is not a valid integer
+    #         notification = anvil.Notification("Please enter a valid amount!", style="warning")
+    #         notification.show()
+    #         return
+
+    #     # Fetch the user's data from the wallet table
+    #     user_data = app_tables.wallet.search(customer_id=customer_id)
+
+    #     if user_data and len(user_data) > 0:
+    #         # If the row exists, update the e_wallet value
+    #         user_row = user_data[0]
+    #         user_row['e_wallet'] = e_wallet_amount
+    #         print("Updating existing row...")
+    #     else:
+    #         # If the row doesn't exist, add a new row
+    #         app_tables.wallet.add_row(customer_id=customer_id, e_wallet=e_wallet_amount)
+    #         print("Adding new row...")
+            
+    #     # Display a success message after updating or adding the e_wallet value
+    #     notification = anvil.Notification("Money Deposited successfully!", style="success")
+    #     notification.show()
+    # else:
+    #     # Notify if no amount is entered
+    #     notification = anvil.Notification("Please enter an amount!", style="warning")
+    #     notification.show()
 
 
   
