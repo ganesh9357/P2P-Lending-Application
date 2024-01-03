c164270d5875b23aa8ed371439a51abdb6e1f0fc
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-01-03 04:30:24 +0000

    Edited settings and admin.dashboard.manage_products.view_products_and_categories.RowTemplate28

--- client_code/admin/dashboard/manage_products/view_products_and_categories/RowTemplate28/__init__.py
+++ client_code/admin/dashboard/manage_products/view_products_and_categories/RowTemplate28/__init__.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+#from .EditDetailsForm import EditDetailsForm
 
 class RowTemplate28(RowTemplate28Template):
   def __init__(self, **properties):
