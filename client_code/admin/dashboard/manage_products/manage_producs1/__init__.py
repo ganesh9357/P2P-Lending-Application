3eb88342fca7f6796e28cc1412df10b145425660
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-01-05 07:53:41 +0000

    Edited settings and admin.dashboard.manage_products.manage_producs1

--- client_code/admin/dashboard/manage_products/manage_producs1/__init__.py
+++ client_code/admin/dashboard/manage_products/manage_producs1/__init__.py
@@ -129,6 +129,7 @@ class manage_producs1(manage_producs1Template):
             Notification("Please enter a valid value for First EMI Payment (>= 1)").show()
             return
         first_emi_payment = int(first_emi_payment)
+        min_months = int(self.min_moths.text)
  
         if self.radio_button_3.selected:
             # Code to execute when radio_button_3 is selected
@@ -140,10 +141,10 @@ class manage_producs1(manage_producs1Template):
             # Code to execute when neither radio_button_3 nor radio_button_4 is selected
             discount_coupons = None  # or any default value
         
-        if product_group == "" or product_name == "" or product_categories == "" or membership_type == "" or processing_fee == "" or extension_fee == "" or interest_type == "" or max_amount == "" or min_amount == "" or min_tenure == "" or max_tenure == "" or roi == "" or emi_payment == "" or discount_coupons == "":
+        if product_group == "" or product_name == "" or product_categories == "" or membership_type == "" or processing_fee == "" or extension_fee == "" or interest_type == "" or max_amount == "" or min_amount == "" or min_tenure == "" or max_tenure == "" or roi == "" or emi_payment == "" or min_months == "" or discount_coupons == "":
             Notification("Fill All Required Details").show()
         else:
-            anvil.server.call('product_details', self.id, product_name, product_group, product_discription, product_categories, processing_fee, extension_fee, membership_type, interest_type, max_amount, min_amount, min_tenure, max_tenure, roi, foreclose_type, foreclosure_fee, extension_allowed, emi_payment, first_emi_payment, discount_coupons)
+            anvil.server.call('product_details', self.id, product_name, product_group, product_discription, product_categories, processing_fee, extension_fee, membership_type, interest_type, max_amount, min_amount, min_tenure, max_tenure, roi, foreclose_type, foreclosure_fee, extension_allowed, emi_payment, first_emi_payment, min_months, discount_coupons)
             product_id = self.label_1.text
             # open_form('admin.dashboard.manage_products.add_group',product_id )
         
