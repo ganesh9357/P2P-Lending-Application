4d60f998b1f668ebfc0fb5a97b77ad26f44c6344
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-01-05 12:42:16 +0000

    Edited bank_users.borrower_dashboard.new_loan_request.loan_type

--- client_code/bank_users/borrower_dashboard/new_loan_request/loan_type/__init__.py
+++ client_code/bank_users/borrower_dashboard/new_loan_request/loan_type/__init__.py
@@ -24,45 +24,7 @@ class loan_type(loan_typeTemplate):
         open_form('bank_users.borrower_dashboard.new_loan_request')
 
     def button_3_click(self, **event_args):
-        loan_amount = self.loan_amount_tb.text
-        tenure = self.tenure_dd.selected_value
-        one_time = self.radio_button_1.selected
-        monthly_emi = self.radio_button_2.selected
-    
-        # Validate loan_amount
-        if not loan_amount:
-            self.label_22.text = "Please fill the loan amount"
-            self.label_22.foreground = '#FF0000'
-        elif not loan_amount.isdigit():
-            self.label_22.text = "Please enter only numeric values (0-9) for loan amount"
-            self.label_22.foreground = '#FF0000'
-        else:
-            min_amount, max_amount = self.get_min_max_amount()
-            loan_amount = int(loan_amount)
-    
-            if min_amount <= loan_amount <= max_amount:
-                self.label_22.text = ""
-            else:
-                self.label_22.text = f"Loan amount should be between {min_amount} and {max_amount}"
-                self.label_22.foreground = '#FF0000'
-    
-        # Validate tenure
-        if not tenure:
-            self.label_23.text = "Please select tenure"
-            self.label_23.foreground = '#FF0000'
-        else:
-            self.label_23.text = ""
-    
-        # Validate radio_button_1 and radio_button_2
-        if not (one_time or monthly_emi):
-            self.label_24.text = "Please select either One-time or Monthly EMI"
-            self.label_24.foreground = '#FF0000'
-        else:
-            self.label_24.text = ""
-    
-        # Proceed to the next form only if all validations pass
-        if not any([self.label_22.text, self.label_23.text, self.label_24.text]):
-            open_form('bank_users.borrower_dashboard.new_loan_request.check_out_form', self.proctct_g, self.prodct_cate, str(loan_amount), tenure, self.user_id)
+      open_form('bank_users.borrower_dashboard.new_loan_request.check_out_form', self.proctct_g, self.prodct_cate, str(loan_amount), tenure, self.user_id)
 
     def fetch_product_data(self):
         return app_tables.product_details.search(
@@ -107,3 +69,49 @@ class loan_type(loan_typeTemplate):
 
     def label_18_show(self, **event_args):
         self.display_label_text(self.label_18, 'membership_type')
+
+    def button_4_click(self, **event_args):
+        loan_amount = self.loan_amount_tb.text
+        tenure = self.text_box_1.text
+        one_time = self.radio_button_1.selected
+        monthly_emi = self.radio_button_2.selected
+        three_month = self.radio_button_3.selected
+        six_month = self.radio_button_4.selected
+    
+        # Validate loan_amount
+        if not loan_amount:
+            self.label_22.text = "Please fill the loan amount"
+            self.label_22.foreground = '#FF0000'
+        elif not loan_amount.isdigit():
+            self.label_22.text = "Please enter only numeric values (0-9) for loan amount"
+            self.label_22.foreground = '#FF0000'
+        else:
+            min_amount, max_amount = self.get_min_max_amount()
+            loan_amount = int(loan_amount)
+    
+            if min_amount <= loan_amount <= max_amount:
+                self.label_22.text = ""
+            else:
+                self.label_22.text = f"Loan amount should be between {min_amount} and {max_amount}"
+                self.label_22.foreground = '#FF0000'
+    
+        # Validate tenure
+        if not tenure:
+            self.label_23.text = "Please select tenure"
+            self.label_23.foreground = '#FF0000'
+        else:
+            self.label_23.text = ""
+    
+        # Validate radio_button_1 and radio_button_2
+        if not (one_time or monthly_emi or three_month or six_month):
+            self.label_24.text = "Please select either One-time or Monthly EMI"
+            self.label_24.foreground = '#FF0000'
+        else:
+            self.label_24.text = ""
+    
+        # Proceed to the next form only if all validations pass
+        if not any([self.label_22.text, self.label_23.text, self.label_24.text]):
+          self.grid_panel_2.visible = True
+          self.button_2.visible = True
+          self.button_3.visible = True
+          self.button_4.visible = False
