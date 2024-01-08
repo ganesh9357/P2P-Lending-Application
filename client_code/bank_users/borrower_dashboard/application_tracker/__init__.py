f01334b904e974b00f3f639e12aa124f361369f7
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-01-08 09:41:08 +0000

    Edited bank_users.borrower_dashboard.application_tracker

--- client_code/bank_users/borrower_dashboard/application_tracker/__init__.py
+++ client_code/bank_users/borrower_dashboard/application_tracker/__init__.py
@@ -18,11 +18,11 @@ class application_tracker(application_trackerTemplate):
     # Any code you write here will run before the form oopens.
     under_process_items = app_tables.loan_details.search(loan_updated_status=q.like('under proces%'), borrower_customer_id=self.user_id)
     self.repeating_panel_5.items = under_process_items
-    self.label_1.text = str(len(under_process_items))
+    #self.label_1.text = str(len(under_process_items))
 
     approved_items = app_tables.loan_details.search(loan_updated_status=q.like('approve%'), borrower_customer_id=self.user_id)
     self.repeating_panel_1.items = approved_items
-    self.label_1.text = str(len(approved_items))      
+    #self.label_2.text = str(len(approved_items))      
   
   def home_borrower_registration_button_click(self, **event_args):
     open_form('bank_users.borrower_dashboard')
