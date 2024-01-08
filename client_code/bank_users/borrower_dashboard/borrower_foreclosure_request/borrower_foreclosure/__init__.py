5086184640c391599ee485d3a052b1d7fd53d015
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-01-08 05:27:26 +0000

    Edited settings and bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure

--- client_code/bank_users/borrower_dashboard/borrower_foreclosure_request/borrower_foreclosure/__init__.py
+++ client_code/bank_users/borrower_dashboard/borrower_foreclosure_request/borrower_foreclosure/__init__.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime
 
 class borrower_foreclosure(borrower_foreclosureTemplate):
     def __init__(self, selected_row, **properties):
@@ -18,7 +19,6 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
         self.label_loan_tenure.text = f"{selected_row['tenure']} Months"
         self.label_interest_rate.text = f"{selected_row['interest_rate']} % pa"
         self.label_credit_limit.text = f"{selected_row['credit_limit']}"
-        self.label_tpm.text = f"{selected_row['borrower_last_payment_done']}"
         self.label_3.text = "Foreclosure Request Under Process......"  
         self.label_5.text = "Foreclosure Request Rejected"
 
@@ -77,20 +77,33 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
       selected_row = self.selected_row
       loan_id = selected_row['loan_id']
 
-      borrower_first_payment_done = selected_row['borrower_first_payment_done']
+      loan_details_row = app_tables.loan_details.get(loan_id=loan_id)
       borrower_last_payment_done = selected_row['borrower_last_payment_done']
+      borrower_first_payment_done = loan_details_row['borrower_first_payment_done']
       product_id = selected_row['product_id']
       product_details_row = app_tables.product_details.get(product_id=product_id)
       min_months = product_details_row['min_months']
       min_months = int(min_months)
       print(min_months)
 
+      # Calculate the difference in months between first and last payments
+      months_difference = self.calculate_month_difference(borrower_first_payment_done, borrower_last_payment_done)
+      months_difference = int(months_difference)
+      print(months_difference)
 
-      if borrower_last_payment_done > min_months:
+      self.label_tpm.text = f"{months_difference} months"
+
+
+      if  months_difference >= min_months:
             open_form('bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure.foreclose', selected_row=selected_row)
       else:
             alert('You are not eligible for foreclosure! You have to pay at least 12 months.')
             open_form('bank_users.borrower_dashboard.borrower_foreclosure_request')
+
+    @staticmethod
+    def calculate_month_difference(start_date, end_date):
+        months_difference = (end_date.year - start_date.year) * 12 + end_date.month - start_date.month
+        return months_difference
         
     def button_2_click(self, **event_args):
         """This method is called when the button is clicked"""
